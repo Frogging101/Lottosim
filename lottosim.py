420edf877cac59745695f91daf55fcf92924ffc0
John Brooks <john@fastquake.com>
2015-06-18 19:32:21 +0000

    Add bonus number functionality

--- lottosim.py
+++ lottosim.py
@@ -1,13 +1,17 @@
 import random
 import re
+from LottoObjects import *
 
 low = 1
 high = 49
 numChoices = 6
 plays = 1000
+useBonusNumber = True
 
 def genNumbers(exclude=set()):
     numbers = set()
+    wn = WinningNumbers()
+
     while True:
         while len(numbers) < numChoices:
             randNum = random.randrange(low,high+1)
@@ -17,14 +21,35 @@ def genNumbers(exclude=set()):
         else:
             #print str(numbers)+" excluded!"
             numbers = set()
-    return numbers
+    if useBonusNumber:
+        while True:
+            randNum = random.randrange(low,high+1)
+            if randNum not in numbers:
+                wn.bonus = randNum
+                break
+
+    wn.numbers = numbers
+    return wn
+
+def genChoices(exclude=set()):
+    choices = genNumbers(exclude).numbers
+    return choices
 
 def compareNumbers(choices, winners):
     matches = 0
+    match = Match()
     for choice in choices:
-        if choice in winners:
+        if choice in winners.numbers:
             matches += 1
-    return matches
+
+    match.matches = matches
+
+    if useBonusNumber and winners.bonus:
+        if winners.bonus in choices:
+            match.bonusMatched = True
+        else:
+            match.bonusMatched = False
+    return match
 
 def getUserInt(question, default):
     while True:
@@ -55,6 +80,7 @@ def getUserYN(question, default):
             return default
 
 def setOptions():
+    global numChoices, useBonusNumber, high, low
     reNumRange = re.compile(r"(?P<low>[0-9]+)-(?P<high>[0-9]+)")
     while True:
         strNumRange = raw_input("What is the range of numbers to choose from? [1-49]: ")
@@ -69,6 +95,7 @@ def setOptions():
         break
 
     numChoices = getUserInt("How many numbers should be chosen? [6]: ", 6)
+    useBonusNumber = getUserYN("Generate a bonus number? [True]: ", True)
 
 class Menu:
     #TODO: Fix this numbering thing
@@ -120,69 +147,85 @@ while not done:
         winningNumbers = genNumbers()
         while again:
             alreadyPlayed = set()
-            print "\nWinning numbers: "+str(winningNumbers)
+            print "\nWinning numbers: "+str(winningNumbers.numbers)
+            if useBonusNumber:
+                print "Bonus: "+str(winningNumbers.bonus)
             plays = getUserInt("How many plays should be run? ["+str(plays)+"]: ", plays)
 
             print '\n',
 
-            allMatches = [0] * (numChoices+1)
+            allMatches = [[0,0] for x in range(numChoices+2)]
 
             for play in range(plays):
-                testNumbers = genNumbers(alreadyPlayed)
-                numMatches = compareNumbers(testNumbers, winningNumbers)
+                testNumbers = genChoices(alreadyPlayed)
+                match = compareNumbers(testNumbers, winningNumbers)
                 alreadyPlayed.add(frozenset(testNumbers))
-                allMatches[numMatches] += 1
+                allMatches[match.matches][0] += 1
+                allMatches[match.matches][1] += int(match.bonusMatched)
 
             for i,matches in enumerate(allMatches):
-                print str(i) + " matched: " + str(matches)
-
+                print str(i) + " matched: " + str(allMatches[i][0]),
+                if useBonusNumber:
+                    print ', '+str(allMatches[i][1])+" with bonus"
+                print '\n',
             again = getUserYN("Run again? (y/n): ", True)
 
     elif selection == Menu.PUNTILWIN:
         numToWin = 6
+        bonusRequired = False
         again = True
         while again:
             alreadyPlayed = set()
             winningNumbers = genNumbers()
             plays = 0
-            print "\nWinning numbers: "+str(winningNumbers)
+            print "\nWinning numbers: "+str(winningNumbers.numbers)
+            if useBonusNumber:
+                print "Bonus number: "+str(winningNumbers.bonus)
             numToWin = getUserInt("How many numbers are required to win? ["+str(numToWin)+"]: ", numToWin)
+            if useBonusNumber:
+                bonusRequired = getUserYN("Bonus required to win? ["+str(bonusRequired)+"]: ", bonusRequired)
             valid = True
             if numToWin == 0:
                 print "\nYou win by default"
                 valid = False
-            elif numToWin > numChoices:
+            elif numToWin+int(bonusRequired) > numChoices:
                 print "\nYou're never going to win this"
                 valid = False
 
             if valid:
                 win = False
                 while not win:
-                    testNumbers = genNumbers(alreadyPlayed)
-                    numMatches = compareNumbers(testNumbers, winningNumbers)
+                    testNumbers = genChoices(alreadyPlayed)
+                    match = compareNumbers(testNumbers, winningNumbers)
                     alreadyPlayed.add(frozenset(testNumbers))
                     plays += 1
                     if plays % 1000000 == 0:
                         print str(plays) + " plays performed"
-                    if numMatches >= numToWin:
-                        print "\nYou won by matching "+str(numMatches)+" numbers after "+str(plays)+" plays"
-                        print "Your numbers were "+str(testNumbers)
+                    if match.matches >= numToWin:
                         win = True
+                    if bonusRequired and not match.bonusMatched and match.matches < numToWin+1:
+                        win = False
+                    if win:
+                        print "\nYou won by matching "+str(match.matches)+" numbers after "+str(plays)+" plays"
+                        print "Your numbers were "+str(testNumbers)
             again = getUserYN("Run again? (y/n): ", True)
 
     elif selection == Menu.MULTIDRAW:
         numToWin = 6
         again = True
         plays = 10
+        bonusRequired = False
         while again:
             draws = 0
             numToWin = getUserInt("\nHow many numbers are required to win? ["+str(numToWin)+"]: ", numToWin)
+            if useBonusNumber:
+                bonusRequired = getUserYN("Bonus required to win? (y/n) ["+str(bonusRequired)+"]: ", bonusRequired)
             plays = getUserInt("How many plays will you do per draw? ["+str(plays)+"]: ", plays)
             valid = True
             if numToWin == 0:
                 print "\nYou win by default"
                 valid = False
-            elif numToWin > numChoices:
+            elif numToWin+int(bonusRequired) > numChoices:
                 print "\nYou're never going to win this"
                 valid = False
 
@@ -197,13 +240,19 @@ while not done:
                         print str(draws) + " draws performed"
 
                     for x in range(plays):
-                        testNumbers = genNumbers(alreadyPlayed)
-                        numMatches = compareNumbers(testNumbers, winningNumbers)
+                        testNumbers = genChoices(alreadyPlayed)
+                        match = compareNumbers(testNumbers, winningNumbers)
                         alreadyPlayed.add(frozenset(testNumbers))
-                        if numMatches >= numToWin:
-                            print "\nYou won by matching "+str(numMatches)+" numbers after "+str(draws)+" draws"
-                            print "Your numbers were "+str(testNumbers)+" and the winning numbers were "+str(winningNumbers)
+                        if match.matches >= numToWin:
                             win = True
+                        if bonusRequired and not match.bonusMatched and match.matches < numToWin+1:
+                            win = False
+                        if win:
+                            print "\nYou won by matching "+str(match.matches)+" numbers after "+str(draws)+" draws"
+                            print "Your numbers were "+str(testNumbers)+" and the winning numbers were "+str(winningNumbers.numbers)
+                            if useBonusNumber:
+                                print "The bonuns number was "+str(winningNumbers.bonus)
+                            break
 
             again = getUserYN("Run again? (y/n): ", True)
 
